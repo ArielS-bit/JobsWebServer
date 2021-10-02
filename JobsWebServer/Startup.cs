b556764b35f07c7dd7205b187295c149a373ea32
Ariel Shoham <ariel.shoham400@gmail.com>
2021-10-02 18:47:28 +0300

    Update Startup.cs

--- JobsWebServer/Startup.cs
+++ JobsWebServer/Startup.cs
@@ -7,18 +7,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration; //This one adds the GetConnectionString method to Configuration
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JobsWebServerBL.Models;
+
 
 namespace JobsWebServer
 {
     public class Startup
     {
+        //Constructor need to be added to get an instance of configuration file (appsettings.json)
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //Add Controllers and set the Json Serializer to handle loop referencing
+            services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions
+                        .ReferenceHandler = ReferenceHandler.Preserve);
 
-             
+            #region Add Session Support
+            //The following two commands set the Session state to work!
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(180);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            #endregion
+
+            //The following set the connection string to the DB and DB context!
+            #region Add DB Context Support
+            string connectionString = this.Configuration.GetConnectionString("IJobDB");
+
+            services.AddDbContext<IJobDBContext>(options => options
+                                                                .UseSqlServer(connectionString));
+                                                                //.UseLazyLoadingProxies());
+            #endregion
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,16 +66,16 @@ namespace JobsWebServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles(); //Added to have the wwwroot folder and server to accept calls to static files
             app.UseRouting();
+            app.UseSession(); //Tell the server to use sessions!
+
 
             app.UseEndpoints(endpoints =>
             {
                 //Map all routings for controllers 
                 endpoints.MapControllers();
-                //endpoints.MapGet("/", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello World!");
-                //});
+               
             });
         }
     }
