ed0f4519d98167ce0437c0d526a284531113092b
User <User@LAB2-7>
2021-10-11 14:58:31 +0300

    MoreChanges

--- JobsWebServer/Controllers/IJobController.cs
+++ JobsWebServer/Controllers/IJobController.cs
@@ -8,12 +8,12 @@ using Microsoft.EntityFrameworkCore;
 using JobsWebServerBL.Models;
 using System.IO;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace JobsWebServer.Controllers
 {
     [Route("iJobAPI")]
     [ApiController]
+    
     public class IJobController : ControllerBase
     {
         //Connection to the DB Context via dependency injection
@@ -46,47 +46,9 @@ namespace JobsWebServer.Controllers
         [HttpGet]
         public DateTime GetTime()
         {
-           return DateTime.Now;
+            return DateTime.Now;
 
         }
 
-
-
-
-
-
-
-
-        //// GET: api/<IJobController>
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET api/<IJobController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<IJobController>
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT api/<IJobController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/<IJobController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
