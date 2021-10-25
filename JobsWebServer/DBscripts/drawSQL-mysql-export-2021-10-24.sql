0839479a00eb22f06a10eb29b0a419611ea2ac3a
User <User@LAB2-7>
2021-10-25 12:47:16 +0300

    DB changes

--- JobsWebServer/DBscripts/drawSQL-mysql-export-2021-10-24.sql
+++ JobsWebServer/DBscripts/drawSQL-mysql-export-2021-10-24.sql
@@ -1,8 +1,17 @@
-use master
+--Finished DB
 
-create database IJobDB
+USE master
 
-use IJobDB
+CREATE DATABASE IJobDB 
+
+GO
+
+USE master	
+
+USE IJobDB
+
+Go
+
 
 CREATE TABLE Employees(
     EmployeeID INT PRIMARY KEY IDENTITY(1000,1) NOT NULL ,
@@ -11,11 +20,18 @@ CREATE TABLE Employees(
     RatingID INT NOT NULL
 );
 
+CREATE UNIQUE INDEX UserIDIndex
+on Employees (UserID);
+
+
 CREATE TABLE Employers(
-    EmployerID INT PRIMARY KEY IDENTITY(2000,1) NOT NULL ,
+    EmployerID INT PRIMARY KEY NOT NULL ,
     UserID INT NOT NULL,
     IsEmployee bit NOT NULL
 );
+
+CREATE UNIQUE INDEX UserIDIndex
+on Employers (UserID);
 
 CREATE TABLE JobOffers(
     JobOfferID INT PRIMARY KEY NOT NULL ,
@@ -29,26 +45,38 @@ CREATE TABLE JobOffers(
     CommentID INT NOT NULL
 );
 
+CREATE UNIQUE INDEX JobOfferStatusIDIndex
+on JobOffers (JobOfferStatusID);
+
+CREATE UNIQUE INDEX EmployerIDIndex
+on JobOffers (EmployerID);
+
 CREATE TABLE Categories(
-    CategoryID INT PRIMARY KEY NOT NULL ,
+    CategoryID INT PRIMARY KEY IDENTITY(1,1) NOT NULL ,
     CategoryName VARCHAR(255) NOT NULL
 );
 
+CREATE UNIQUE INDEX CategoryNameIndex
+on Categories (CategoryName);
+
 CREATE TABLE Comments(
-    CommentID INT PRIMARY KEY NOT NULL ,
+    CommentID INT PRIMARY KEY IDENTITY(10000,1) NOT NULL ,
     Content TEXT NOT NULL,
     JobOfferID INT NOT NULL,
     JobRequestID INT NOT NULL,
     Likes INT NOT NULL
 );
 
+CREATE UNIQUE INDEX JobOfferIDIndex
+on Comments (JobOfferID);
+--Change to plural and in the forgein as well
 CREATE TABLE Rating(
     RatingID INT PRIMARY KEY NOT NULL ,
     RatingName VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE JobRequests(
-    JobRequestID INT PRIMARY KEY NOT NULL ,
+    JobRequestID INT PRIMARY KEY IDENTITY(10000,1) NOT NULL ,
     Topic VARCHAR(255) NOT NULL,
     Content VARCHAR(255) NOT NULL,
     IsApplied bit NOT NULL,
@@ -59,37 +87,50 @@ CREATE TABLE JobRequests(
     JobOfferStatusID INT NOT NULL
 );
 
+CREATE UNIQUE INDEX EmployeeIDIndex
+on JobRequests (EmployeeID);
+
 CREATE TABLE ChatBox(
-    PhraseID INT PRIMARY KEY NOT NULL,
+    PhraseID INT PRIMARY KEY IDENTITY(10,1) NOT NULL,
     Content VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE InterstedInRequest(
-    ID INT PRIMARY KEY NOT NULL ,
+    ID INT PRIMARY KEY IDENTITY(10,1) NOT NULL ,
     JobRequestID INT NOT NULL,
     EmployerID INT NOT NULL
 );
 
 CREATE TABLE JobApplications(
-    AppID INT PRIMARY KEY NOT NULL ,
+    AppID INT PRIMARY KEY IDENTITY(100000,1) NOT NULL ,
     EmployeeID INT NOT NULL,
     JobOfferID INT NOT NULL,
     JobAppStatusID INT NOT NULL,
     EmployerID INT NOT NULL
 );
 
+CREATE UNIQUE INDEX AppStatusIndex
+on JobApplications (JobAppStatusID);
+
 CREATE TABLE JobApplicationStatus(
-    StatusID INT PRIMARY KEY NOT NULL ,
+    StatusID INT PRIMARY KEY IDENTITY(1,1) NOT NULL ,
     StatusName VARCHAR(255) NOT NULL
 );
 
+CREATE UNIQUE INDEX StatusNameIndex
+on JobApplicationStatus (StatusName);
+
+
 CREATE TABLE JobOfferStatus(
-    JobOfferStatusID INT PRIMARY KEY NOT NULL ,
+    JobOfferStatusID INT PRIMARY KEY IDENTITY(1,1) NOT NULL ,
     JobOfferStatus VARCHAR(255) NOT NULL
 );
 
+CREATE UNIQUE INDEX JobOfferStatusIndex
+on JobOfferStatus (JobOfferStatus);
+
 CREATE TABLE Users(
-    UserID INT PRIMARY KEY IDENTITY(100,1) NOT NULL,
+    UserID INT PRIMARY KEY IDENTITY(100000,1) NOT NULL,
     FirstName VARCHAR(255) NOT NULL,
     LastName VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL,
@@ -99,19 +140,28 @@ CREATE TABLE Users(
     Gender VARCHAR(255) NOT NULL,
     UserTypeID INT NOT NULL
 );
+
+CREATE UNIQUE INDEX LastNameIndex
+on Users (LastName);
+
+CREATE UNIQUE INDEX EmailIndex
+on Users (Email);
+
 CREATE TABLE UserTypes(
-    UserTypeID INT PRIMARY KEY NOT NULL ,
+    UserTypeID INT PRIMARY KEY IDENTITY(1,1) NOT NULL ,
     UserTypeName VARCHAR(255) NOT NULL
 );
+
+USE IJobDB
 
 ALTER TABLE
     JobApplications ADD CONSTRAINT jobapplications_employeeid_foreign FOREIGN KEY(EmployeeID) REFERENCES Employees(EmployeeID);
 ALTER TABLE
-    JobRequests ADD CONSTRAINT jobrequests_employeeid_foreign FOREIGN KEY(EmployeeID) REFERENCES Employees(EmployeeID);
+	JobRequests ADD CONSTRAINT jobrequests_employeeid_foreign FOREIGN KEY(EmployeeID) REFERENCES Employees(EmployeeID);
 ALTER TABLE
     Employees ADD CONSTRAINT employees_ratingid_foreign FOREIGN KEY(RatingID) REFERENCES Rating(RatingID);
 ALTER TABLE
-    JobRequests ADD CONSTRAINT jobrequests_employerid_foreign FOREIGN KEY(EmployerID) REFERENCES Employers(EmployerID);
+	JobRequests ADD CONSTRAINT jobrequests_employerid_foreign FOREIGN KEY(EmployerID) REFERENCES Employers(EmployerID);
 ALTER TABLE
     JobApplications ADD CONSTRAINT jobapplications_jobofferid_foreign FOREIGN KEY(JobOfferID) REFERENCES JobOffers(JobOfferID);
 ALTER TABLE
@@ -123,13 +173,13 @@ ALTER TABLE
 ALTER TABLE
     JobOffers ADD CONSTRAINT joboffers_categoryid_foreign FOREIGN KEY(CategoryID) REFERENCES Categories(CategoryID);
 ALTER TABLE
-    JobRequests ADD CONSTRAINT jobrequests_categoryid_foreign FOREIGN KEY(CategoryID) REFERENCES Categories(CategoryID);
+    JobRequests ADD CONSTRAINT jobrequest_categoryid_foreign FOREIGN KEY(CategoryID) REFERENCES Categories(CategoryID);
 ALTER TABLE
     Comments ADD CONSTRAINT comments_jobrequestid_foreign FOREIGN KEY(JobRequestID) REFERENCES JobRequests(JobRequestID);
 ALTER TABLE
-    JobRequests ADD CONSTRAINT jobrequests_commentid_foreign FOREIGN KEY(CommentID) REFERENCES Comments(CommentID);
+    JobRequests ADD CONSTRAINT jobrequest_commentid_foreign FOREIGN KEY(CommentID) REFERENCES Comments(CommentID);
 ALTER TABLE
-    JobRequests ADD CONSTRAINT jobrequests_jobofferstatusid_foreign FOREIGN KEY(JobOfferStatusID) REFERENCES JobOfferStatus(JobOfferStatusID);
+    JobRequests ADD CONSTRAINT jobrequest_jobofferstatusid_foreign FOREIGN KEY(JobOfferStatusID) REFERENCES JobOfferStatus(JobOfferStatusID);
 ALTER TABLE
     InterstedInRequest ADD CONSTRAINT interstedinrequest_jobrequestid_foreign FOREIGN KEY(JobRequestID) REFERENCES JobRequests(JobRequestID);
 ALTER TABLE
@@ -137,8 +187,124 @@ ALTER TABLE
 ALTER TABLE
     JobApplications ADD CONSTRAINT jobapplications_jobappstatusid_foreign FOREIGN KEY(JobAppStatusID) REFERENCES JobApplicationStatus(StatusID);
 ALTER TABLE
-    JobOffers ADD CONSTRAINT joboffers_jobofferstatusid_foreign FOREIGN KEY(JobOfferStatusID) REFERENCES JobOfferStatus(JobOfferStatusID);
+    JobOffers ADD CONSTRAINT joboffer_jobofferstatusid_foreign FOREIGN KEY(JobOfferStatusID) REFERENCES JobOfferStatus(JobOfferStatusID);
 ALTER TABLE
     Users ADD CONSTRAINT users_usertypeid_foreign FOREIGN KEY(UserTypeID) REFERENCES UserTypes(UserTypeID);
 
+--Creating Rating Table
+ USE [IJobDB]
+GO
 
+INSERT INTO [dbo].[Rating]
+           ([RatingID]
+           ,[RatingName])
+     VALUES
+           (1,
+		   'Bad')
+GO
+
+INSERT INTO [dbo].[Rating]
+           ([RatingID]
+           ,[RatingName])
+     VALUES
+           (2,
+		   'Not bad')
+GO
+
+
+INSERT INTO [dbo].[Rating]
+           ([RatingID]
+           ,[RatingName])
+     VALUES
+           (3,
+		   'Good')
+GO
+
+INSERT INTO [dbo].[Rating]
+           ([RatingID]
+           ,[RatingName])
+     VALUES
+           (4,
+		   'Very good')
+GO
+
+
+INSERT INTO [dbo].[Rating]
+           ([RatingID]
+           ,[RatingName])
+     VALUES
+           (5,
+		   'Excellent')
+GO
+
+
+--Creating UserTypes Table
+USE [IJobDB]
+GO
+
+INSERT INTO [dbo].[UserTypes]
+           ([UserTypeName])
+     VALUES
+           ('Admin')
+GO
+
+INSERT INTO [dbo].[UserTypes]
+           ([UserTypeName])
+     VALUES
+           ('Employer')
+GO
+
+INSERT INTO [dbo].[UserTypes]
+           ([UserTypeName])
+     VALUES
+           ('Employee')
+GO
+
+SELECT * FROM UserTypes
+
+
+--Creating Users Table
+USE [IJobDB]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Pass]
+           ,[Nickname]
+           ,[Birthday]
+           ,[Gender]
+           ,[UserTypeID])
+     VALUES
+           ('Lucas',
+           'Haribo',
+           'lucas@gmail.com',
+           '12',
+           'Luc1',
+           '2016/08/27 21:02:44',
+           'Male',
+           '2')
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Pass]
+           ,[Nickname]
+           ,[Birthday]
+           ,[Gender]
+           ,[UserTypeID])
+     VALUES
+           ('Steve',
+           'Jobs',
+           'steve@gmail.com',
+           'steve12',
+           'AppleFounder',
+           '1955/02/24 21:02:44',
+           'Male',
+           '1')
+GO
+
+SELECT * FROM Users
